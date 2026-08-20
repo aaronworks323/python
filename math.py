27e6a4eb5d6f8c33ee69b1053dff5466775e106a
aaronpraise <aaronworks323@gmail.com>
2026-08-20 12:18:35 +0530

    add math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+import math
+print(math.pi)
+print(round(math.pi))
+
+
+x = 64
+print(math.sqrt(x))
+
+y = 9.7
+z = 9.7
+print(math.ceil(y))
+print(math.floor(z))
+
+radius = int(input("Enter the radius of the circle: "))
+pi = math.pi
+circumfrence = (2 * pi * radius)
+print(round((circumfrence), 2))
